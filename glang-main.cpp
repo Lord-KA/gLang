ac49186adb3bb75d07829a5f196560131f8f62c3
Lord <lord.nemo@protonmail.com>
2022-02-05 22:45:42 +0300

    created basic getopt interface

--- glang-main.cpp
+++ glang-main.cpp
@@ -1,12 +1,49 @@
 #include "glang.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    gLang langStruct;
-    gLang *ctx = &langStruct;
-    gLang_ctor(ctx, stderr);
+    int c = 0;
+    char *inFileName  = NULL;
+    char *outFileName = NULL;
 
-    FILE *in = stdin;
+    while ((c = getopt(argc, argv, "hi:o:")) != -1) {
+        switch(c) {
+        case 'h':
+            printf("This is a custom compiler for gLang made by _Lord_KA_:\n"
+                   "Usage: -h          : show this help\n"
+                   "       -i FileName : input from file\n"
+                   "       -o FileName : output to file\n");
+            return 0;
+        case 'o':
+            outFileName = optarg;
+            break;
+        case 'i':
+            inFileName = optarg;
+            break;
+        }
+    }
+
+    FILE *in  = stdin;
+    FILE *out = stdout;
+    if (outFileName != NULL) {
+        out = fopen(outFileName, "w");
+        if (out == NULL) {
+            fprintf(stderr, "Failed to open the file!\n");
+            return 1;
+        }
+    }
+    if (inFileName != NULL) {
+        in = fopen(inFileName, "r");
+        if (in == NULL) {
+            fprintf(stderr, "Failed to open the file!\n");
+            return 1;
+        }
+    }
+
+    #ifdef EXTRA_VERBOSE
+        fprintf(stderr, "inFileName = %s, outFileName = %s\n", inFileName, outFileName);
+    #endif
+
     fseek(in, 0L, SEEK_END);
     size_t bufLen = ftell(in);
     fseek(in, 0L, SEEK_SET);
@@ -15,28 +52,44 @@ int main()
 
     if (ferror(in)) {
         fprintf(stderr, "Failed to read from file!\n");
+        free(buffer);
         return 1;
     }
 
-    size_t len = strlen(buffer);
-    fprintf(stderr, "len = %lu\n", len);
+    #ifdef EXTRA_VERBOSE
+        size_t len = strlen(buffer);
+        fprintf(stderr, "len = %lu\n", len);
+    #endif
+
+    gLang langStruct;
+    gLang *ctx = &langStruct;
+    gLang_ctor(ctx, stderr);
+
     gLang_lexer(ctx, buffer);
-    FILE *out = fopen("before.gv", "w");
-    gTree_dumpPoolGraphViz(&ctx->tree, out);
-    fclose(out);
+    #ifdef EXTRA_VERBOSE
+        FILE *gvOut = fopen("before.gv", "w");
+        gTree_dumpPoolGraphViz(&ctx->tree, gvOut);
+        fclose(gvOut);
+    #endif
 
     gLang_parser(ctx);
-    out = fopen("after.gv", "w");
-    gTree_dumpPoolGraphViz(&ctx->tree, out);
-    fclose(out);
+    #ifdef EXTRA_VERBOSE
+        gvOut = fopen("after.gv", "w");
+        gTree_dumpPoolGraphViz(&ctx->tree, gvOut);
+        fclose(gvOut);
+    #endif
 
     gLang_optimize(ctx, ctx->tree.root);
-    out = fopen("optimized.gv", "w");
-    gTree_dumpPoolGraphViz(&ctx->tree, out);
+    #ifdef EXTRA_VERBOSE
+        gvOut = fopen("optimized.gv", "w");
+        gTree_dumpPoolGraphViz(&ctx->tree, gvOut);
+        fclose(gvOut);
+    #endif
+
+    gLang_compile(ctx, out);
+
+finish:
     fclose(out);
-
-    gLang_compile(ctx, stdout);
-
     gLang_dtor(ctx);
     free(buffer);
 }
